68a31dc0d2be337b64d84713efd9c74ae2891696
mrezafiqri <mrezafqrnnr@gmail.com>
2024-01-06 20:25:08 +0800

    merge

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -37,7 +37,7 @@ const main = async () => {
 
   searchMovieElement.clickEvent = onButtonSearchClicked;
 
-  console.log('COde with Reza 123')
+  console.log('COde with Reza 12233')
 };
 
 export default main;
